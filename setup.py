560438f913fb64c7b63f06765095cc0a738c22ce
nwg-piotr <nwg.piotr@gmail.com>
2020-01-13 11:06:17 +0100

    bump to 0.2.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(f_name):
 
 setup(
     name='sgtk-menu',
-    version='0.2.1',
+    version='0.2.2',
     description='GTK menu for sway and i3',
     packages=['sgtk-menu'],
     include_package_data=True,
