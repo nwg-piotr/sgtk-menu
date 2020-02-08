557a91ac637142564976c0d8e0f9ac2e66aca5c8
piotr <nwg.piotr@gmail.com>
2020-02-08 15:59:46 +0100

    window name fixed

--- sgtk_menu/bar.py
+++ sgtk_menu/bar.py
@@ -33,9 +33,9 @@ wm = check_wm()
 # This will apply to the overlay window; we can't do so outside the config file on i3.
 # We'll do it for i3 by applying commands to the focused window in open_menu method.
 if wm == "sway":
-    var = subprocess.run(['swaymsg', 'for_window', '[title=\"~sgtk-grid\"]', 'floating', 'enable'],
+    var = subprocess.run(['swaymsg', 'for_window', '[title=\"~sgtk*\"]', 'floating', 'enable'],
                          stdout=subprocess.DEVNULL).returncode == 0
-    var = subprocess.run(['swaymsg', 'for_window', '[title=\"~sgtk-grid\"]', 'border', 'none'],
+    var = subprocess.run(['swaymsg', 'for_window', '[title=\"~sgtk*\"]', 'border', 'none'],
                          stdout=subprocess.DEVNULL).returncode == 0
 
 other_wm = not wm == "sway" and not wm == "i3"
