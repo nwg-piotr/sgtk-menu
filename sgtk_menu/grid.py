de3c7271fc0738ad50452925ad92d86d7fe7b96d
piotr <nwg.piotr@gmail.com>
2020-02-19 02:53:41 +0100

    optimization to list_entries()

--- sgtk_menu/grid.py
+++ sgtk_menu/grid.py
@@ -365,17 +365,21 @@ def list_entries():
 
                                 if line.startswith('Name='):
                                     _name = line.split('=')[1].strip()
+                                    continue
 
                                 if line.startswith(loc_name):
                                     _name = line.split('=')[1].strip()
+                                    continue
 
                                 loc_comment = 'Comment{}='.format(locale)
 
                                 if line.startswith('Comment='):
                                     _comment = line.split('=')[1].strip()
+                                    continue
 
                                 if line.startswith(loc_comment):
                                     _comment = line.split('=')[1].strip()
+                                    continue
 
                                 if line.startswith('Exec='):
                                     cmd = line.split('=')[1:]
@@ -383,6 +387,8 @@ def list_entries():
                                     _exec = c.strip()
                                     if '%' in _exec:
                                         _exec = _exec.split('%')[0].strip()
+                                    continue
+
                                 if line.startswith('Icon='):
                                     _icon = line.split('=')[1].strip()
 
